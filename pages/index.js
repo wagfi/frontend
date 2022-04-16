d3c37baa7915bafe66a8f420de64151b56bbbe94
oraclown <owen.burton13@gmail.com>
2022-04-16 18:27:26 -0400

    Add tailwindcss dependency

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
